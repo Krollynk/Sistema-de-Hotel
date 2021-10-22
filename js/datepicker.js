5d500d06d614db92cf649d0ede41b6a3e1220aae
Krollynk <87242234+Krollynk@users.noreply.github.com>
2021-10-22 10:47:49 +0200

    a

---
+++ js/datepicker.js
@@ -0,0 +1,43 @@
+$(function(){
+    $("#datepicker").datepicker({
+        dateFormat: 'yy-mm-dd',
+        minDate: new Date()
+    });
+})
+
+$(function(){
+    $("#datepicker2").datepicker({
+        dateFormat: 'yy-mm-dd',
+        minDate: new Date()
+    });
+})
+
+function calcular_dias(){
+    var fecha_inicio = new Date(document.getElementById('datepicker').value);
+    var fecha_fin = new Date(document.getElementById('datepicker2').value);
+    const fecha = new Date().toLocaleDateString();
+    var dias = (parseInt((fecha_fin - fecha_inicio)/(1000*60*60*24)));
+
+    console.log(fecha_inicio);
+    console.log(fecha_fin);
+    console.log(fecha);
+    console.log(dias);
+    
+    return dias;
+}
+
+function calcular_precio(){
+    var dias = calcular_dias();
+    var precio = parseInt(document.getElementById('precio').textContent);
+
+    var precio_final = precio * dias;
+    console.log(precio);
+    console.log(precio_final);
+
+    return precio_final;
+}
+
+$('#datepicker2').change(function(){
+    $('#edadCalculada').text(calcular_dias());
+    $('#subtotal').text("Q" + calcular_precio());
+});
